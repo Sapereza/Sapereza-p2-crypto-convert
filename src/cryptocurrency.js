2793588308af7aaef64dbc95bde4cd6f32e03eaf
Sapereza <s.perezanderson@gmail.com>
2022-08-09 13:20:03 -0400

    added some more css on the body and background areas of sections to make it look more like the example. Used the same colors for the background and switch button by using the color picker extension

---
+++ src/cryptocurrency.js
@@ -0,0 +1,5 @@
+var usersInput = document.getElementById('userInput');
+var bitcoinName = document.getElementById('crypto');
+var worldCurrency = document.getElementById('money');
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}","./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        switch: '#1070E0',
+        sectionBg: '#F8F8F8',
+        titleArea: '#F9FBFE',
+        bodyBg: '#FFFFFF',
+      }
+    },
   },
   plugins: [],
 }
