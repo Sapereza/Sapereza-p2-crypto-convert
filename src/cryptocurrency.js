1d60d14db98809028f681a8bee6e87e0b55f6c87
Sapereza <s.perezanderson@gmail.com>
2022-08-10 15:15:29 -0400

    Added a script that lets you select the bitcoin type and currency type and that selection is then displayed in the output below. But an optioin needs to be selected in order for it to be displayed.

--- src/cryptocurrency.js
+++ src/cryptocurrency.js
@@ -1,5 +1,13 @@
-var usersInput = document.getElementById('userInput');
-var bitcoinName = document.getElementById('crypto');
-var worldCurrency = document.getElementById('money');
+var cryptoType = document.getElementById("cryptoType");
+var nameOfBitcoin = document.getElementById("nameOfBitcoin");
+var money = document.getElementById("money");
+var nameOfWorldCurrency = document.getElementById("nameOfWorldCurrency");
 
 
+cryptoType.onchange = function(){
+    nameOfBitcoin.innerHTML = [this.value];
+}
+
+money.onchange = function(){
+    nameOfWorldCurrency.innerHTML = [this.value];
+}
